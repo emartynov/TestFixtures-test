3b10f81796b12159db1a7e8478a99f802d8ea9d6
Eugen Martynov <eugenemartynov@gmail.com>
2022-01-05 20:46:20 +0100

    Add test for the kotlin test fixtures

--- core/src/test/java/cz/skywall/multimoduleexample/core/mapper/UserMapperKtTest.kt
+++ core/src/test/java/cz/skywall/multimoduleexample/core/mapper/UserMapperKtTest.kt
@@ -1,11 +1,15 @@
 package cz.skywall.multimoduleexample.core.mapper
 
 import org.junit.Test
-import cz.skywall.multimoduleexample.database.Fixture
+import cz.skywall.multimoduleexample.database.UserDBFixtures
+// kotlin is not supported yet
+//import cz.skywall.multimoduleexample.database.UserDBKotlinFixtures
 
 class UserMapperKtTest {
     @Test
     fun `Some test`() {
-        val user = Fixture.defaultUser
+        val user = UserDBFixtures.defaultUser
+//        kotlin is not supported yet
+//        val kotlinUser = UserDBKotlinFixtures.defaultUser
     }
 }
